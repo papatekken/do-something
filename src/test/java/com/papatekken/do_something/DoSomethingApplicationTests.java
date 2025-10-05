b393368f877cab02eb1bdbc56982ec447c47d765
Allan Yip <papatekken@gmail.com>
2025-10-05 02:49:21 +0100

    add springboot backbone

---
+++ src/test/java/com/papatekken/do_something/DoSomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papatekken.do_something;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoSomethingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
