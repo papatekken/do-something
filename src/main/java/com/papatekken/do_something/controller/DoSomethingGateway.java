3247d457fa66b7a7906314d290df1e6ad1ed8813
Allan Yip <papatekken@gmail.com>
2025-10-12 18:04:52 +0100

    prepare controller

--- src/main/java/com/papatekken/do_something/controller/DoSomethingGateway.java
+++ src/main/java/com/papatekken/do_something/controller/DoSomethingGateway.java
@@ -1,5 +1,13 @@
 package com.papatekken.do_something.controller;
 
+import org.springframework.validation.annotation.Validated;
+
+@RestController
+@Validated
 public class DoSomethingGateway {
 
+    @GetMapping("/greeting")
+    public ResponseEntity<String> greeting(){
+        return ResponseEntity.ok("hello world");
+    }
 }
